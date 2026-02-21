a3e1be4f998963b4da862573498b12a308248d8f
Abhishek Dubey <abhishekdubey331@gmail.com>
2026-02-21 19:42:34 +0530

    fix: adjust dynamic relative execution path to ../.env to match flattened bundler output in dist directory

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -9,7 +9,7 @@ import { fileURLToPath } from 'url';
 try {
     const __filename = fileURLToPath(import.meta.url);
     const __dirname = path.dirname(__filename);
-    const envPath = path.resolve(__dirname, '../../.env');
+    const envPath = path.resolve(__dirname, '../.env');
 
     if (fs.existsSync(envPath)) {
         const parsed = dotenv.parse(fs.readFileSync(envPath, { encoding: 'utf8' }));
